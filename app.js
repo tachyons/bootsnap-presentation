3b6b319259b38dbdb6130259c2f0bd94410f4bf4
Aboobacker MK <aboobackervyd@gmail.com>
2021-05-24 09:55:05 +0530

    Fix stylesheet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import HighLight from 'reveal.js/plugin/highlight/highlight.js'
 import Notes from 'reveal.js/plugin/notes/notes.js'
+import "reveal.js/dist/theme/black.css"
 import "reveal.js/dist/reveal.css"
 import "reveal.js/plugin/highlight/monokai.css"
 
